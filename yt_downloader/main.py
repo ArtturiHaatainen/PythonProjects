44586c0178c38a73c1487b57e29a4cb7a82ac310
Artturi Haatainen <AD1557@student.jamk.fi>
2025-02-24 22:32:01 +0200

    progressbar added, not working properly

--- yt_downloader/main.py
+++ yt_downloader/main.py
@@ -5,15 +5,26 @@ from pytubefix import YouTube
 def start_download():
     try:
         yt_link = link.get()    
-        yt_object = YouTube(yt_link)
+        yt_object = YouTube(yt_link, on_progress_callback=on_progress)
         video = yt_object.streams.get_highest_resolution()
-        print(f"Downloading {yt_object.title}...")
-        video.download(output_path="C:/Users/Artturi/Downloads")
-        print("Download completed")
-    except Exception as e:
-        print(f"Error: {e}")
-        print("YouTube link may not be valid or there is a problem with the internet connection")
 
+        title.configure(text=yt_object.title)
+        dl_finished.configure(text="")
+        video.download(output_path="C:/Users/Artturi/Downloads")
+        dl_finished.configure(text="Download finished!")
+    except Exception as e:
+        dl_finished.configure(text="YouTube link may not be valid or there is a problem with the internet connection", text_color="red")
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage = (bytes_downloaded / total_size) * 100
+    dl_progressbarp.configure(text=f"{round(percentage)}%")
+    dl_progressbarp.update()
+
+    dl_progressbar.set(percentage)
+    dl_progressbar.update()
 # Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -34,7 +45,14 @@ link.pack(padx=20, pady=20)
 dl_button = customtkinter.CTkButton(app, text="Download", font=("Arial", 15), command=start_download)
 dl_button.pack(padx=20, pady=20)
 
+dl_progressbar = customtkinter.CTkProgressBar(app, width=400)
+dl_progressbar.set(0)
+dl_progressbar.pack(padx=20, pady=20)
 
+dl_progressbarp = customtkinter.CTkLabel(app, text="0%", font=("Arial", 15))
+dl_progressbarp.pack(padx=20, pady=20)
 
+dl_finished = customtkinter.CTkLabel(app, text="", font=("Arial", 15))
+dl_finished.pack(padx=20, pady=20)
 # Run app
 app.mainloop()
